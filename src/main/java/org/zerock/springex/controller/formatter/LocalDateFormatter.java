80ba09674db43cd83cf9ba673452d00a296da549
Soonrang <clearsbyul30@naver.com>
2023-08-11 18:33:13 +0900

    commit date 0811

--- src/main/java/org/zerock/springex/controller/formatter/LocalDateFormatter.java
+++ src/main/java/org/zerock/springex/controller/formatter/LocalDateFormatter.java
@@ -1,15 +1,19 @@
 package org.zerock.springex.controller.formatter;
 import org.springframework.format.Formatter;
+import org.springframework.web.bind.annotation.GetMapping;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
 public class LocalDateFormatter implements Formatter<LocalDate> {
 
+
     @Override
     public LocalDate parse(String text, Locale locale) {
         return LocalDate.parse(text, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
     }
+
 
     @Override
     public String print(LocalDate object, Locale locale) {
--- src/main/webapp/WEB-INF/views/custom404.jsp
+++ src/main/webapp/WEB-INF/views/custom404.jsp
@@ -1,16 +1,16 @@
 <%--
   Created by IntelliJ IDEA.
   User: kosmo
-  Date: 2023-08-09
-  Time: 오후 5:55
+  Date: 2023-08-11
+  Time: 오후 5:14
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>hello</title>
+    <title>Title</title>
 </head>
 <body>
-<h1> 안뇽! </h1>
+<h1>Oops! 페이지를 찾을 수 없습니다!</h1>
 </body>
 </html>
